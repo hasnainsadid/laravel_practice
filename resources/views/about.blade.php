7a0afb7fc91d40747ac3767403bc43658d6f8583
Your NameMamun876 <you@example.commamunrrashid876@gmail.com>
2023-12-26 13:30:58 +0600

    day1

---
+++ resources/views/about.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('title', 'About Page')
+
+@section('content')
+
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-sm-12">
+      <h2>TITLE HEADING</h2>
+      <h5>Title description, Dec 26, 2023</h5>
+      <div class="fakeimg">Fake Image</div>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+
+      <h2 class="mt-5">TITLE HEADING</h2>
+      <h5>Title description, Sep 21, 2023</h5>
+      <div class="fakeimg">Fake Image</div>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    </div>
+  </div>
+</div>
+
+@endsection
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('title', 'About Page')
+
+@section('content')
+
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-sm-12">
+      <form method="post" action="send">
+        @csrf
+        <div class="row">
+          <div class="col">
+            <input type="text" class="form-control" placeholder="Enter First Name" name="f_name">
+          </div>
+          <div class="col">
+            <input type="text" class="form-control" placeholder="Enter First Name" name="l_name">
+          </div>
+        </div>
+        <input type="text" class="form-control" placeholder="Enter Subject" name="sub">
+        <textarea class="form-control" rows="5" name="msg"></textarea>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  </div>
+</div>
+
+@endsection
